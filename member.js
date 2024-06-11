7ef36a9cdbffa035e47b497c65d1bc8603c4da04
Rahul Nair <rn@rahul-nair.com>
2024-06-11 07:29:48 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,6 @@
+function skillsMember() {
+  var member = document.getElementById("member");
+  member.style.display = "none";
+  var skills = document.getElementById("skills");
+  skills.style.display = "block";
+}
